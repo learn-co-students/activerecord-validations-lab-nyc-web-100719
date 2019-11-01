e4fa54c8871def0d2d4342ee72e717997dd8922e
LainaJ <lainakarosic@gmail.com>
2019-11-01 12:26:17 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true 
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: %w{Fiction Non-fiction} }
+    validates :title, exlusion: {in: %w {Won/'t Believe Secret Top Guess}}
 end
